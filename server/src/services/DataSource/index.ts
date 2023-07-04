6c1b11382939c102fd7ef5c64144898e0d80b2ec
Santiago Volentiera <santiagovolentiera@gmail.com>
2023-07-04 19:17:18 -0300

    feat: added dotenv

--- server/src/services/DataSource/index.ts
+++ server/src/services/DataSource/index.ts
@@ -1,14 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { entities } from "./entities";
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "alcaraz",
-  database: "wallet",
+  type: process.env.DB_TYPE as any,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT as unknown as number,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: entities,
   synchronize: true,
   logging: false
