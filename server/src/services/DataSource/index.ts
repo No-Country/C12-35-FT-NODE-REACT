761ec12200a1bfa0a551964982a8e80bcc249d6e
Santiago Volentiera <santiagovolentiera@gmail.com>
2023-07-06 10:53:11 -0300

    fix: dotenv

--- server/src/services/DataSource/index.ts
+++ server/src/services/DataSource/index.ts
@@ -3,17 +3,23 @@ dotenv.config();
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { entities } from "./entities";
-import { EnvVariables } from "./DataSource.interface";
+import { EnvVariables } from "./interface";
 
-const envVariables: EnvVariables = process.env as unknown as EnvVariables;
+const envVariables: EnvVariables = {
+  DB_HOST: process.env.DB_HOST || "localhost",
+  DB_USERNAME: process.env.DB_USERNAME || "postgres",
+  DB_PASSWORD: process.env.DB_PASSWORD || "",
+  DB_NAME: process.env.DB_NAME || "postgres",
+  DB_PORT: process.env.DB_PORT || "5432"
+};
 
-export const AppDataSource = new DataSource({
+export const AppDataSource: DataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: envVariables.DB_PORT,
+  host: envVariables.DB_HOST,
+  port: parseInt(envVariables.DB_PORT),
   username: envVariables.DB_USERNAME,
   password: envVariables.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: envVariables.DB_NAME,
   entities: entities,
   synchronize: true,
   logging: false
