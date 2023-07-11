5ee68b271320880d574ac757df6e73c19d5c0f0f
Tomás Alcaraz <tomasalejandroalcaraz@gmail.com>
2023-07-11 13:13:55 -0300

    endpoint login

--- server/src/services/Login/routes/index.ts
+++ server/src/services/Login/routes/index.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import EncrypService from "../../Encryp";
 import DBUser from "../../../components/users/data";
-import { IUser } from "../../../components/users/models/IUser";
 
 const router = Router();
 
@@ -9,21 +8,12 @@ router.post("/login", async (req: any, res: any) => {
   try {
     const { email, password } = req.body;
     const user: any = await DBUser.findOne({ email });
+    if (!user) throw new Error("el email no está registrado");
     const validation = await EncrypService.validate(password, user.password);
+    if (!validation) throw new Error("la contraseña es inválida");
     res.status(200).json({ auth: true, user, validation });
   } catch (error: any) {
-    res.status(400).json({ auth: false, error: error });
-  }
-});
-
-router.post("/signin", async (req: any, res: any) => {
-  try {
-    const { email, password } = req.body;
-    const user: any = await DBUser.findOne({ email });
-    const validation = await EncrypService.validate(password, user.password);
-    res.status(200).json({ auth: true, user, validation });
-  } catch (error: any) {
-    res.status(400).json({ auth: false, error: error });
+    res.status(400).json({ auth: false, error: error.message });
   }
 });
 
