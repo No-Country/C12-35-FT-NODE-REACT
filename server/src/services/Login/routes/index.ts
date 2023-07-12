901ad6b076660ae501253ac8467573e34e63642d
Tomás Alcaraz <tomasalejandroalcaraz@gmail.com>
2023-07-12 17:18:05 -0300

    error

--- server/src/services/Login/routes/index.ts
+++ server/src/services/Login/routes/index.ts
@@ -4,14 +4,14 @@ import DBUser from "../../../components/users/data";
 
 const router = Router();
 
-router.post("/login", async (req: any, res: any) => {
+router.post("/", async (req: any, res: any) => {
   try {
     const { email, password } = req.body;
     const user: any = await DBUser.findOne({ email });
     if (!user) throw new Error("el email no está registrado");
     const validation = await EncrypService.validate(password, user.password);
     if (!validation) throw new Error("la contraseña es inválida");
-    res.status(200).json({ auth: true, user, validation });
+    res.status(200).json({ auth: true, user });
   } catch (error: any) {
     res.status(400).json({ auth: false, error: error.message });
   }
