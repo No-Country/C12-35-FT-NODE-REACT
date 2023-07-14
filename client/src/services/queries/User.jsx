9912e668f6633995cb1c4ff7ce62ae34e0f21ba5
Lautaro Aboueid <lautaronajib@gmail.com>
2023-07-13 21:20:47 -0300

    fix barrel

--- client/src/services/queries/User.jsx
+++ client/src/services/queries/User.jsx
@@ -1,37 +1,35 @@
-import user from "@@/config/endpoints/User"
+import { User } from "@@/config/endpoints"
 import Cookies from "js-cookie"
 import axios from "axios"
 import { redirect } from "react-router-dom"
 
-const { login, register } = user
+const { login, register } = User
 
 //
 
-const setCookiesAndRedirect = (accessTokenValue ='test1234')=>{
-      Cookies.set('accessToken',accessTokenValue,{expires: 1, path: '/'})
-      window.location.reload();
-      return redirect('/app');
+const setCookiesAndRedirect = (accessTokenValue = "test1234") => {
+  Cookies.set("accessToken", accessTokenValue, { expires: 1, path: "/" })
+  window.location.reload()
+  return redirect("/app")
 }
 
-export const registerUser = (values , setState ) => {
+export const registerUser = (values, setState) => {
   axios
     .post(register, values)
     .then((res) => {
       console.log(res)
       setState()
-      setCookiesAndRedirect()     
-
+      setCookiesAndRedirect()
     })
     .catch((err) => {
       //eliminar al deployar el back
       setCookiesAndRedirect()
       console.log(err)
       setState(err.response.data.data)
-      
     })
 }
 
-export const loginUser = (values , setState ) => {
+export const loginUser = (values, setState) => {
   axios
     .post(login, values)
     .then((res) => {
