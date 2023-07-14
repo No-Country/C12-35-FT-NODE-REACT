10d47da8444d3f3c76716b31e904cd664c572a9b
Santiago Volentiera <santiagovolentiera@gmail.com>
2023-07-14 12:31:26 -0300

    fix: build

--- server/dist/src/utils/response.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function response(res, status, data) {
-    return res.status(status).json({ error: false, data });
-}
-exports.default = response;
