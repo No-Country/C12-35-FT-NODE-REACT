e52ae826b93f9951c75c80dc07f1726b0f8717a4
Lautaro Aboueid <lautaronajib@gmail.com>
2023-07-17 18:48:00 -0300

    fundsAccountSHORTCUT done rev 1.0

---
+++ client/src/components/DetailFund/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const DetailFundWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  font-size: 1.5rem;
+`
+
+export const Fund = styled.h4`
+  font-size: 2.6rem;
+  font-weight: 400;
+`
+
+export const Icon = styled.div``
