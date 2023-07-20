c518f320985c2b5ef2ab65f07e20ff81cfa37e0b
Santiago Volentiera <santiagovolentiera@gmail.com>
2023-07-20 19:18:41 -0300

    .

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -18,16 +18,16 @@ router.use("/transaction-history", transactionHistoryRouter);
 router.use("/payment-gateway", paymentGatewayRouter);
 router.use("/cards", cardRouter);
 router.use("/login", LoginRoutes);
-router.post("/checkout", async (req, res) => {
-  const algo = await MercadoPago.createPreference(req.body.amount, req.body.email);
-  //console.log(algo);
-  res.send(algo);
-});
-router.get("/algo", async (req, res) => {
-  const otro = await MercadoPago.getPreference("1298391875-267b65e2-c940-4a16-8414-b359a8654d5f");
-  console.log(otro);
-  res.send(otro);
-});
+// router.post("/checkout", async (req, res) => {
+//   const algo = await MercadoPago.createPreference(req.body.amount, req.body.email);
+//   //console.log(algo);
+//   res.send(algo);
+// });
+// router.get("/algo", async (req, res) => {
+//   const otro = await MercadoPago.getPreference("1298391875-267b65e2-c940-4a16-8414-b359a8654d5f");
+//   console.log(otro);
+//   res.send(otro);
+// });
 // router.get("/success", (req, res) => {res.send("Success");});
 // router.get("/pending", (req, res) => {res.send("Pending");});
 // router.post("/webhook", async (req, res) => {
