b6103fb97dbab755072e4a028fd0ba1fe298bff8
Lautaro Aboueid <lautaronajib@gmail.com>
2023-07-20 21:59:22 -0300

    example names in recent activity

--- client/src/modules/Home/RecentActivity/RecentActivity.jsx
+++ client/src/modules/Home/RecentActivity/RecentActivity.jsx
@@ -9,73 +9,85 @@ export default function RecentActivity() {
       id: 1,
       type: "transfer",
       time: "2023-07-19T16:11:28.000Z",
-      amount: 5906
+      amount: 5906,
+      userDestination: "Marcelo Tinelli"
     },
     {
       id: 2,
       type: "transfer",
       time: "2023-07-19T16:11:28.000Z",
-      amount: 5901
+      amount: 5901,
+      userDestination: "Juan Sosa"
     },
     {
       id: 3,
       type: "send",
       time: "2023-07-19T16:06:28.000Z",
-      amount: 1202
+      amount: 1202,
+      userDestination: "Juan Sosa"
     },
     {
       id: 4,
       type: "send",
       time: "2023-07-19T16:11:28.000Z",
-      amount: 1190
+      amount: 1190,
+      userDestination: "Roberto Aguilar"
     },
     {
       id: 5,
       type: "send",
       time: "2023-07-19T16:10:28.000Z",
-      amount: 59
+      amount: 59,
+      userDestination: "Matias Perez"
     },
     {
       id: 6,
       type: "send",
       time: "2021-08-31T17:32:28.000Z",
-      amount: 52
+      amount: 52,
+      userDestination: "Julieta Sosa"
     },
     {
       id: 7,
       type: "transfer",
       time: "2021-08-31T17:32:28.000Z",
-      amount: 123
+      amount: 123,
+      userDestination: "Augusto Alfonoso"
     },
     {
       id: 8,
       type: "transfer",
       time: "2021-08-31T17:32:28.000Z",
-      amount: 590
+      amount: 590,
+      userDestination: "Alberto Fernandez"
     },
     {
       id: 9,
       type: "transfer",
       time: "2021-08-31T17:32:28.000Z",
-      amount: 1000
+      amount: 1000,
+      userDestination: "Javier Milei"
     },
     {
       id: 10,
       type: "transfer",
       time: "2021-08-31T17:32:28.000Z",
-      amount: 1150
+      amount: 1150,
+      userDestination: "Hugo Moyano"
     },
     {
       id: 11,
       type: "transfer",
       time: "2021-08-31T17:32:28.000Z",
-      amount: 1150
+      amount: 1150,
+      userDestination: "Javier Rodriguez"
     },
     {
       id: 12,
       type: "transfer",
       time: "2021-08-31T17:32:28.000Z",
-      amount: 590
+      amount: 590,
+      userDestination: "Pedro Sanchez"
     }
   ]
   return (
@@ -83,9 +95,15 @@ export default function RecentActivity() {
       <Wrap>
         <ShowMore title='Actividad' link='/activity' />
         <ListActivity>
-          {fakeData.map(({ id, type, time, amount }) => {
+          {fakeData.map(({ id, type, time, amount, userDestination }) => {
             return (
-              <ActivityItem key={id} type={type} amount={amount} time={time} />
+              <ActivityItem
+                key={id}
+                type={type}
+                amount={amount}
+                time={time}
+                userDestination={userDestination}
+              />
             )
           })}
         </ListActivity>
