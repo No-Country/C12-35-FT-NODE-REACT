7cbc237a691f8c44cc0ac970d9d5a17eb37166fc
Santiago Volentiera <santiagovolentiera@gmail.com>
2023-07-24 23:34:46 -0300

    feat: added api doc

---
+++ server/src/routes/documentation.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.redirect("https://documenter.getpostman.com/view/24550006/2s946chaB1");
+});
+
+export default router;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -7,7 +7,8 @@ import paymentGatewayRouter from "../components/paymentGateways/routes";
 import cardRouter from "../components/cards/routes";
 import authRoutes from "../services/Auth/routes";
 import LoginRoutes from "../services/Login/routes";
-import MercadoPago from "../services/MercadoPago/index";
+//import MercadoPago from "../services/MercadoPago/index";
+import docRoutes from "./documentation.routes";
 
 const router = Router();
 
@@ -18,23 +19,7 @@ router.use("/transaction-history", transactionHistoryRouter);
 router.use("/payment-gateway", paymentGatewayRouter);
 router.use("/cards", cardRouter);
 router.use("/login", LoginRoutes);
-// router.post("/checkout", async (req, res) => {
-//   const algo = await MercadoPago.createPreference(req.body.amount, req.body.email);
-//   //console.log(algo);
-//   res.send(algo);
-// });
-// router.get("/algo", async (req, res) => {
-//   const otro = await MercadoPago.getPreference("1298391875-267b65e2-c940-4a16-8414-b359a8654d5f");
-//   console.log(otro);
-//   res.send(otro);
-// });
-// router.get("/success", (req, res) => {res.send("Success");});
-// router.get("/pending", (req, res) => {res.send("Pending");});
-// router.post("/webhook", async (req, res) => {
-//   console.log(req.body);
-//   await MercadoPago.webhook(req.body);
-//   res.send("ok");
-// });
+router.use("/doc", docRoutes);
 
 // la ruta /auth es únicamente para el testeo y entendimiento de tokens
 router.use("/auth", authRoutes);
