a2b13d50db4658687eaa3b86a4624795830c7f4b
Juan José Farina <juanjosefarina.jjf@gmail.com>
2023-07-25 02:07:00 -0300

    first version addMoney

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import { ButtonWrapper } from "./style"
 
-export default function Button({ children, secondary }) {
+export default function Button({ onClick, children, secondary }) {
   //constants
   const isSecondary = secondary ? true : false
 
-  return <ButtonWrapper $secondary={isSecondary}>{children}</ButtonWrapper>
+  return <ButtonWrapper onClick={onClick} $secondary={isSecondary}>{children}</ButtonWrapper>
 }
--- client/src/modules/Home/ManageFunds/ManageFunds.jsx
+++ client/src/modules/Home/ManageFunds/ManageFunds.jsx
@@ -1,13 +1,24 @@
-import React from "react"
-import { ManageFundsWrap, ManageFundsWrapper } from "./style"
-import { Button } from "@/components"
+import React from "react";
+import { ManageFundsWrap, ManageFundsWrapper } from "./style";
+import { Button } from "@/components";
+import { useNavigate } from "react-router-dom";
 
 export default function ManageFunds() {
+  const navigate = useNavigate();
+
+  const addMoney = () => {
+    navigate('./add-money', {replace: false});
+  }
+
+  const sendMoney = () => {
+    navigate('./send-money', {replace: false});
+  }
+
   return (
     <ManageFundsWrapper>
       <ManageFundsWrap>
-        <Button>Ingresar dinero</Button>
-        <Button secondary>Transferir dinero</Button>
+        <Button onClick={addMoney}>Ingresar dinero</Button>
+        <Button secondary onClick={sendMoney}>Transferir dinero</Button>
       </ManageFundsWrap>
     </ManageFundsWrapper>
   )
