d8ceb4160cca02128662fa5a45b1a8f3f9bac5f4
Juan José Farina <juanjosefarina.jjf@gmail.com>
2023-07-25 15:20:51 -0300

    progressing

--- client/src/modules/Home/ManageFunds/ManageFunds.jsx
+++ client/src/modules/Home/ManageFunds/ManageFunds.jsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 import { ManageFundsWrap, ManageFundsWrapper } from "./style";
 import { Button } from "@/components";
-import { useNavigate } from "react-router-dom";
-=======
-import React from "react"
-import { ManageFundsWrap, ManageFundsWrapper } from "./style"
-import { Button } from "@/components"
-import { Link } from "react-router-dom"
->>>>>>> 4fd6dbdd7f8d802fe980ae15f4026eceee288ee3
+import { useNavigate, Link } from "react-router-dom";
 
 export default function ManageFunds() {
   const navigate = useNavigate();
@@ -18,21 +11,14 @@ export default function ManageFunds() {
   }
 
   const sendMoney = () => {
-    navigate('./send-money', {replace: false});
+    navigate('./transf', {replace: false});
   }
 
   return (
     <ManageFundsWrapper>
       <ManageFundsWrap>
-<<<<<<< HEAD
         <Button onClick={addMoney}>Ingresar dinero</Button>
         <Button secondary onClick={sendMoney}>Transferir dinero</Button>
-=======
-        <Button>Ingresar dinero</Button>
-        <Button secondary>
-          <Link to='/app/transf'>Transferir dinero</Link>
-        </Button>
->>>>>>> 4fd6dbdd7f8d802fe980ae15f4026eceee288ee3
       </ManageFundsWrap>
     </ManageFundsWrapper>
   )
--- client/src/pages/AddMoney/AddMoney.jsx
+++ client/src/pages/AddMoney/AddMoney.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
 import { BackArrow } from './styles';
 import { useNavigate } from 'react-router-dom';
+import { UserTool } from "@/components"
+import { EstructureCards } from '@/modules';
+import { FaUserCircle } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import {
+  MdOutlineAccountBalanceWallet,
+  MdOutlinePermContactCalendar
+} from "react-icons/md"
 
 export default function AddMoney() {
+  const userData = useSelector((state) => state.auth);
   const [step, setStep] = useState(0);
   const navigate = useNavigate();
 
@@ -10,26 +19,89 @@ export default function AddMoney() {
     step > 0 ? setStep((step-1)) : navigate(-1);
   }
 
-  const goNext = () => {
+  const getTitle = (step) => {
+    switch(step) {
+      case 0:
+        return 'Selecciona un método de ingreso';
+      case 1:
+        return '¿Desde dónde querés ingresar dinero?';
+      case 2:
+        return 'Ingresá el monto que deseas ingresar';
+      case 3:
+        return 'Resultado de la operación';
+      default:
+        return 'Error';
+    }
+  }
+
+  const showContent = (step) => {
+    switch(step) {
+      case 0:
+        return <>
+          <UserTool
+            data={{
+              id: 1,
+              icon: <MdOutlineAccountBalanceWallet />,
+              title: "Débito",
+              subtitle: "Sin cargo",
+              onClick: selectCard
+            }}
+          />
+          <UserTool
+            data={{
+              id: 2,
+              icon: <MdOutlineAccountBalanceWallet />,
+              title: "Efectivo"
+            }}
+          />
+        </>;
+      case 1:
+        return <>
+          {userData.cards.length > 0 ? userData.cards.map((card) => {
+            <UserTool
+              data={{
+                id: card.id,
+                title: card.card_number,
+                subtitle: card.expiration_date,
+                onClick: selectAmount
+              }}
+            />
+          }) : 
+          <UserTool
+            data={{
+              id: Math.random(),
+              title: '.... 1234',
+              subtitle: 'Vencimiento 24/24',
+              onClick: selectAmount
+            }}
+          />}
+        </>;
+      case 2:
+        return 'Seleccionar monto';
+      case 3:
+        return 'Resultado de la operación';
+      default:
+        return 'working ?';
+    }
+  }
+
+  const selectCard = () => {
+    setStep((step + 1));
+  }
+
+  const selectAmount = () => {
     setStep((step + 1));
   }
 
   return (
     <div>
         <BackArrow onClick={goBack}>{'<-'}</BackArrow>
-        {step == 0 ? 
-          <div onClick={goNext}>Seleccionar medio de pago
-          </div> :
-        step == 1 ?
-          <div onClick={goNext}>Seleccionar tarjeta
-          </div> :
-        step == 2 ?
-          <div onClick={goNext}>Seleccionar monto
-          </div> : 
-        step == 3 ?
-          <div onClick={goNext}>Resultado de la operación
-          </div> :
-        <></>}
+        <EstructureCards 
+          icon={<FaUserCircle />}
+          title={getTitle(step)}
+        >
+          {showContent(step)}
+        </EstructureCards>
     </div>
   )
 }
