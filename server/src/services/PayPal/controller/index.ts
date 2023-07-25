c7d469584465c1320c738680cb77e82a59ef3619
Tomás Alcaraz <tomasalejandroalcaraz@gmail.com>
2023-07-25 01:20:51 -0300

    paypal service

--- server/src/services/PayPal/controller/index.ts
+++ server/src/services/PayPal/controller/index.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express";
 import PayPalService from "../";
+import { PAYPAL_API } from "../../../config/envs";
 
 export default {
   createOrder: async (req: Request, res: Response) => {
-    const { currency, amount, description } = req.body;
+    const { currency, amount } = req.body;
     try {
-      const response = await PayPalService.createOrder(currency, amount, description);
+      const response = await PayPalService.createOrder(currency, amount);
+      res.status(200).json({ response });
+    } catch (error: any) {
+      res.status(404).json({ error: error.message });
+    }
+  },
+  captureOrder: async (req: Request, res: Response) => {
+    const { token, PlayerID } = req.query;
+    try {
+      const url = `${PAYPAL_API}/v2/checkout/orders/${token}/capture`;
+
+      const response = await PayPalService.captureOrder(url);
       res.status(200).json({ response });
     } catch (error: any) {
       res.status(404).json({ error: error.message });
