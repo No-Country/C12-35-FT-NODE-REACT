8374148ae84cd3efd27162ba0c5b8bfec13c6cd8
Juan José Farina <juanjosefarina.jjf@gmail.com>
2023-07-26 13:17:29 -0300

    works perfect now

--- client/src/services/queries/User.jsx
+++ client/src/services/queries/User.jsx
@@ -92,8 +92,13 @@ export const verifyUser = createAsyncThunk(
 export const add = createAsyncThunk(
   "post/transactions",
   async (values, {dispatch }) => {
+    const accessToken = Cookies.get("accessToken")
     try {
-      const response = await axios.post(addMoney, values)
+      const response = await axios.post(addMoney, values, {
+        headers: {
+          "access-token": accessToken
+        }
+      })
 
       const data = await response.data
 
