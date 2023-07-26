6fb50fbc5a0b82c6822792309c32daa2f268634c
Tomás Alcaraz <tomasalejandroalcaraz@gmail.com>
2023-07-26 20:15:37 -0300

    .

--- server/src/components/transactionHistories/controllers/index.ts
+++ server/src/components/transactionHistories/controllers/index.ts
@@ -3,6 +3,7 @@ import createHistory from "./createHistory.controller";
 import getHistoryById from "./getHistoryById.controller";
 import updateHistory from "./updateHistory.controller";
 import deleteHistory from "./deleteHistory.controller";
+import accountInfo from "./accountInfo.controller";
 import utils from "../../../utils";
 
 export default {
@@ -10,5 +11,6 @@ export default {
   createHistory: utils.catched(createHistory),
   getHistoryById: utils.catched(getHistoryById),
   updateHistory: utils.catched(updateHistory),
-  deleteHistory: utils.catched(deleteHistory)
+  deleteHistory: utils.catched(deleteHistory),
+  accountInfo: utils.catched(accountInfo)
 };
