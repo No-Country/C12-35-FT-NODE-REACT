d333f58e8b9395175d9dfce0b220f4458cf0f555
Tomás Alcaraz <tomasalejandroalcaraz@gmail.com>
2023-07-27 11:41:20 -0300

    .

--- server/src/components/Account/controllers/getAccountById.controller.ts
+++ server/src/components/Account/controllers/getAccountById.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import response from "../../../utils/response";
 import AccountService from "../services";
 
-export default async (req: Request, res: Response) => {
-  const data = await AccountService.getAccountById(parseInt(req.params.id));
+export default async (req: any, res: Response) => {
+  const data = await AccountService.getAccountById(parseInt(req.userId));
   return response(res, 200, data);
 };
