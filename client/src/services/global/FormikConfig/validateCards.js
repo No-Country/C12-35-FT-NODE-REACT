bbb94888aa2f40dba73b5b3a90f2a572048f9fa7
Lautaro Aboueid <lautaronajib@gmail.com>
2023-07-27 22:59:44 -0300

    fix styles

--- client/src/services/global/FormikConfig/validateCards.js
+++ client/src/services/global/FormikConfig/validateCards.js
@@ -1,14 +1,18 @@
 import * as Yup from "yup"
 
 export const validateCards = () => {
+  const typeCard = ["CREDIT", "DEBIT"]
+
   return {
     validationSchema: Yup.object().shape({
       card_number: Yup.string()
-        .matches(/^\d{16,16}$/, "El número de tarjeta no es válido") // Entre 16 y 19 dígitos numéricos
-        .required("El número de tarjeta es obligatorio"),
+        .required("El número de tarjeta es obligatorio")
+        .max(16, "El número de tarjeta no es válido")
+        .min(16, "El número de tarjeta no es válido"),
       cardholder_name: Yup.string()
         .matches(/^[a-zA-Z ]+$/, "El nombre del titular no es válido") // Solo letras y espacios permitidos
-        .required("El nombre del titular es obligatorio"),
+        .required("El nombre del titular es obligatorio")
+        .max(25, "El nombre del titular no es válido"),
       expiration_date: Yup.string()
         .matches(/^(0[1-9]|1[0-2])\/\d{2}$/, "Fecha de vencimiento no válida") // Formato válido: MM/AA y mes entre 01 y 12
         .test(
@@ -30,8 +34,12 @@ export const validateCards = () => {
         .required("La fecha de vencimiento es obligatoria"),
       cvv: Yup.string()
         .matches(/^\d{3}$/, "CVV no válido") // Exactamente 3 dígitos numéricos
-        .required("El CVV es obligatorio"),
-      type: Yup.string().required("El tipo de tarjeta es obligatorio")
+        .required("El CVV es obligatorio")
+        .max(3, "CVV no válido"),
+      type: Yup.string()
+        .required("El tipo de tarjeta es obligatorio")
+        .oneOf(typeCard, `Solo puede ser ${typeCard.join(", ")}`)
+        .required("Debes seleccionar una opción válida.")
     })
   }
 }
