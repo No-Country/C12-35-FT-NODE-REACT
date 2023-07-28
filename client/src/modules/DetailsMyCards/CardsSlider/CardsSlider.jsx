98dd96e60b3c75844cd8b3ea0fdb16d80374f01f
Lautaro Aboueid <lautaronajib@gmail.com>
2023-07-27 22:40:09 -0300

    fix cards and implements home

--- client/src/modules/DetailsMyCards/CardsSlider/CardsSlider.jsx
+++ client/src/modules/DetailsMyCards/CardsSlider/CardsSlider.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { Slider, SliderWrapper } from "./style";
-import { CardPageMyCards } from "@/components";
-import { useCards } from "./hook";
-import { useSelector } from 'react-redux'
+import React from "react"
+import { Slider, SliderWrapper } from "./style"
+import { CardPageMyCards } from "@/components"
+import { useCards } from "./hook"
+import { useSelector } from "react-redux"
 
 function CardsSlider() {
-  const { handleMouseDown, handleMouseLeave, handleMouseUp, handleMouseMove } = useCards();
+  const { handleMouseDown, handleMouseLeave, handleMouseUp, handleMouseMove } =
+    useCards()
   const cards = useSelector((state) => state.auth.cards)
-  
+
   return (
     <SliderWrapper
       onMouseDown={handleMouseDown}
       onMouseLeave={handleMouseLeave}
       onMouseUp={handleMouseUp}
-      onMouseMove={handleMouseMove}
-    >
+      onMouseMove={handleMouseMove}>
       <Slider>
         {cards.map((el) => (
-          <CardPageMyCards cards={el} key={el} />
+          <CardPageMyCards cards={el} key={el?.id} />
         ))}
       </Slider>
     </SliderWrapper>
-  );
+  )
 }
 
-export default CardsSlider;
+export default CardsSlider
--- client/src/modules/DetailsMyCards/CardsSlider/style.js
+++ client/src/modules/DetailsMyCards/CardsSlider/style.js
@@ -1,38 +1,37 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const Slider = styled.div`
   display: flex;
   position: absolute;
   left: 1rem;
-`;
+`
 export const SliderWrapper = styled.div`
   display: flex;
   justify-content: center;
-  overflow: hidden;
+  overflow-x: hidden;
   width: 100%;
   height: 9.4rem;
-  background-color: ;
   position: relative;
-  cursor:pointer;
-`;
+  cursor: pointer;
+`
 export const MyCardsWrapper = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
   gap: 1rem;
   padding: 1rem;
-`;
+`
 export const ButtonWraper = styled.div`
   display: flex;
   justify-content: center;
   gap: 1rem;
   border-radius: var(--radius-light);
-`;
+`
 export const TitleWrap = styled.p`
   font-size: 1.2rem;
   padding: 1rem;
   color: #8e8e8e;
-`;
+`
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -40,7 +39,7 @@ export const Wrapper = styled.div`
   padding-top: 0;
   padding-left: 1rem;
   padding-bottom: 1.8rem;
-  padding-right:1.8rem;
+  padding-right: 1.8rem;
   background-color: var(--background-card);
   border-radius: var(--radius-light);
-`;
+`
